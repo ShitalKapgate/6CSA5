17680c1f9e5a250d280ee3ee65d0610112c19998
shita <shita@shitalkapgate>
2023-06-02 11:53:19 +0530

    this is third commit

--- 6CSA5/src/LocatorAssign/LinkTextAssign.java
+++ 6CSA5/src/LocatorAssign/LinkTextAssign.java
@@ -11,6 +11,52 @@ public class LinkTextAssign {
 	    driver.get("https://www.netflix.com");
 	    driver.findElement(By.linkText("Sign In"));
 	    System.out.println("element 1 identified");
+	    
+	    driver.manage().window().maximize();
+	    driver.get("https://www.zomato.com");
+	    driver.findElement(By.linkText("Log in"));
+	    System.out.println("element 2 identified");
+	    
+	   driver.manage().window().maximize();
+	   driver.get("https://www.netflix.com");
+	   driver.findElement(By.linkText("Sign In"));
+	   System.out.println("element 3 identified");
+	   
+	   driver.manage().window().maximize();
+	   driver.get("https://www.fb.com");
+	   driver.findElement(By.linkText("Forgotten password?"));
+	   System.out.println("element 4 identified");
+	   
+	  /* driver.manage().window().maximize();
+	   driver.get("https://www..com");
+	   driver.findElement(By.linkText(""));
+	   System.out.println("element 5 identified");*/
+	   
+	   driver.manage().window().maximize();
+	   driver.get("https://www.amazon.com");
+	   driver.findElement(By.linkText("Best Sellers"));
+	   System.out.println("element 6 identified");
+	   
+	  
+	   driver.manage().window().maximize();
+	   driver.get("https://www.swiggy.com");
+	   driver.findElement(By.linkText("Sign up"));
+	   System.out.println("element 7 identified");
+	   
+	   driver.manage().window().maximize();
+	   driver.get("https://demo.actitime.com/login.do");
+	   driver.findElement(By.linkText("Forgot your password?"));
+	   System.out.println("element 8 identified");
+	   
+	   driver.manage().window().maximize();
+	   driver.get("https://www.flipkart.com");
+	   driver.findElement(By.linkText("Login"));
+	   System.out.println("element 9 identified");
+	   
+	   driver.manage().window().maximize();
+	   driver.get("https://www.zee5.com");
+	   driver.findElement(By.linkText("Web Series"));
+	   System.out.println("element 10 identified");
 		
 	}
 
--- 6CSA5/src/LocatorAssign/PartialLinkTextAssign.java
+++ 6CSA5/src/LocatorAssign/PartialLinkTextAssign.java
@@ -6,18 +6,60 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class PartialLinkTextAssign {
 	public static void main(String[] args) {
-		WebDriver driver = new ChromeDriver();
+		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.get("https://www.netflix.com/in/");
-		driver.findElement(By.partialLinkText("Sign In"));
-		System.out.println("element 1 identified");
-		
-		driver.manage().window().maximize();
+	    driver.get("https://www.netflix.com");
+	    driver.findElement(By.partialLinkText("Sign"));
+	    System.out.println("element 1 identified");
+	    
+	    driver.manage().window().maximize();
+	    driver.get("https://www.zomato.com");
+	    driver.findElement(By.partialLinkText("Log"));
+	    System.out.println("element 2 identified");
+	    
+	   driver.manage().window().maximize();
+	   driver.get("https://www.netflix.com");
+	   driver.findElement(By.partialLinkText("Sign"));
+	   System.out.println("element 3 identified");
+	   
+	   driver.manage().window().maximize();
+	   driver.get("https://www.fb.com");
+	   driver.findElement(By.partialLinkText("Forgotten passwor"));
+	   System.out.println("element 4 identified");
+	   
+	   driver.manage().window().maximize();
 		driver.get("https://demowebshop.tricentis.com/login");
 		driver.findElement(By.partialLinkText("Apparel"));
-		System.out.println("element 2 identified");
-		driver.close();
+		System.out.println("element 5 identified");
+		
 
+	   
+	   /*driver.manage().window().maximize();
+	   driver.get("https://www.amazon.com");
+	   driver.findElement(By.partialLinkText("Best"));
+	   System.out.println("element 6 identified"); */
+	   
+	  
+	   driver.manage().window().maximize();
+	   driver.get("https://www.swiggy.com");
+	   driver.findElement(By.partialLinkText("Sign"));
+	   System.out.println("element 7 identified");
+	   
+	   driver.manage().window().maximize();
+	   driver.get("https://demo.actitime.com/login.do");
+	   driver.findElement(By.partialLinkText("Forgot"));
+	   System.out.println("element 8 identified");
+	   
+	   driver.manage().window().maximize();
+	   driver.get("https://www.flipkart.com");
+	   driver.findElement(By.partialLinkText("Log"));
+	   System.out.println("element 9 identified");
+	   
+	   driver.manage().window().maximize();
+	   driver.get("https://www.zee5.com");
+	   driver.findElement(By.partialLinkText("Web"));
+	   System.out.println("element 10 identified");
+		
 	}
 
 }
